90c2fffc743d5e17ceb10cf79a8e1c00b19e825d
Mauro Moreno <mmauromoreno1@gmail.com>
2022-10-04 16:40:13 -0300

    Dash - Dash system implemented

--- Doctor Vírus/Assets/Scripts/PlayerMovement.cs
+++ Doctor Vírus/Assets/Scripts/PlayerMovement.cs
@@ -9,6 +9,11 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody2D rb;
     
     Vector2 movement;
+
+    private bool canDash = true;
+    private bool isDashing;
+    private float dashingPower = 6f;
+    private float dashingTime = 0.2f;
     
     void Awake()
     {
@@ -23,6 +28,10 @@ public class PlayerMovement : MonoBehaviour
 
     void FixedUpdate()
     {
+        if (isDashing)
+        {
+            return;
+        }
         // Swap sprite direction, wether you're going right or left
         if(movement.x > 0)
         {
@@ -36,8 +45,21 @@ public class PlayerMovement : MonoBehaviour
         // Moves the character
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
 
+        if (Input.GetKeyDown(KeyCode.Mouse1) && canDash)
+        {
+            StartCoroutine(Dash());
+        }
     }
 
+    private IEnumerator Dash()
+    {
+        canDash = false;
+        isDashing = true;
+        rb.velocity = new Vector2(transform.localScale.x * dashingPower, 0f);
+        yield return new WaitForSeconds(dashingTime);
+        isDashing = false;
+    }
+    
     public void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.tag == "Enemy")
