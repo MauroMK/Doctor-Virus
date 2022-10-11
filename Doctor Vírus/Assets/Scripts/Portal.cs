8d18297760878b339b0d11789c9acc825454e769
Mauro Moreno <mmauromoreno1@gmail.com>
2022-10-11 19:49:44 -0300

    Prototype screen done

---
+++ Doctor Vírus/Assets/Scripts/Portal.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portal : MonoBehaviour
+{
+
+    public GameObject prototypeScreen;
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            ShowPrototypeScreen();
+        }
+    }
+
+    public void ShowPrototypeScreen()
+    {
+        prototypeScreen.SetActive(true);
+    }
+}
