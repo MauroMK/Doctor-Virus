fe28bb9e5b5b5eb171607fb52662705f5b2bf7f0
Mauro Moreno <mmauromoreno1@gmail.com>
2022-10-24 15:12:55 -0300

    Weapons - Weapon switch through numbers in keyboard

--- Doctor Vírus/Assets/Scripts/Player/WeaponSwitch.cs
+++ Doctor Vírus/Assets/Scripts/Player/WeaponSwitch.cs
@@ -10,12 +10,18 @@ public class WeaponSwitch : MonoBehaviour
     public bool isImune;
     
     #region Antibiotic
-    
     public PlayerMovement player;
     private float buffTime = 2f;
     private float buffSpeed = 1f;
     private float normalPlayerSpeed = 0.5f;
     public SpriteRenderer spriteShield;
+    #endregion
+
+    #region Sound
+    [SerializeField]
+    private AudioClip SyringuePickupSound;
+    [SerializeField]
+    private AudioClip AlcoholPickupSound;
     #endregion
 
     void Start()
@@ -32,6 +38,7 @@ public class WeaponSwitch : MonoBehaviour
     {
         int previousSelectedWeapon = selectedWeapon;
 
+        // Switch weapons through scrollwheel
         if (Input.GetAxis("Mouse ScrollWheel") > 0f)
         {
             if (selectedWeapon >= transform.childCount - 1)
@@ -54,6 +61,27 @@ public class WeaponSwitch : MonoBehaviour
             {
                 selectedWeapon--;
             }
+        }
+
+        // Switch weapons through numbers
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            selectedWeapon = 0;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2) && transform.childCount >= 2)
+        {
+            selectedWeapon = 1;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha3) && transform.childCount >= 3)
+        {
+            selectedWeapon = 2;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha4) && transform.childCount >= 4)
+        {
+            selectedWeapon = 3;
         }
 
         if (previousSelectedWeapon != selectedWeapon)
